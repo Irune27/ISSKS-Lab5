84f94989daff0669a81b6bd5df32d5c5e6187636
Irune27 <114346207+Irune27@users.noreply.github.com>
2023-11-16 08:48:25 +0100

    Update backupScript.sh

--- backupScript.sh
+++ backupScript.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# Kopiaren jatorria eta helmuga definitu.
+
 jatorria = /home/irune/Segurtasuna
 helmuga = /var/tmp/Backups
 
-# Data lortzea UUUU-HH-EE formatoan.
 data = $(date + %F)
 
-# Direktorioa sortzea data erabiliz.
 mkdir -p $helmuga/$data
 
-# Jatorria eta helmuga sinkronizatu.
 rsync -a --link-dest=$helmuga/$(date -d yesterday + %F) $jatorria/ $jatorria/$data/
